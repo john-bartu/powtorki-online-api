7f40a682b622b7a7d6bcf1433b422548797b088f
jamabaiz <jan@bartulacode.pl>
2026-08-15 11:37:35 +0200

    Startup: simplify local dev

--- startup.py
+++ startup.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", port=5000, log_level="info", host="0.0.0.0", workers=4)
+    uvicorn.run("app.main:app", port=5000, log_level="info", host="0.0.0.0", reload=True)
